e2b87916e073d9eb9159db401acd4e67188e30d8
Georgiy Eresko <zhorik.eresko@mail.ru>
2024-07-09 13:09:20 +0300

    final

--- app/handlers.py
+++ app/handlers.py
@@ -38,32 +38,27 @@ async def invalid_phio(message: Message):
 async def register_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(Register.phone)
-    await message.answer('Укажите Ваш номер телефона\nНомер телефона должен быть в формате 7 999 999 99 99', reply_markup=kb.get_number)
+    await message.answer('Укажите Ваш номер телефона\nНомер телефона должен быть в формате 7 999 999 99 99')
+
+
 
 
 # Обработка Номера
-if Register.phone == Message.text:
-    @router.message(Register.phone, lambda message: not re.fullmatch(r'7 \d{3} \d{3} \d{2} \d{2}', message.text))
-    async def invalid_phone(message: Message):
-        await message.reply("Номер телефона должен быть в формате 7 999 999 99 99. Попробуйте снова:")
+@router.message(Register.phone, lambda message: not re.fullmatch(r'7 \d{3} \d{3} \d{2} \d{2}', message.text))
+async def invalid_phone(message: Message):
+    await message.reply("Номер телефона должен быть в формате 7 999 999 99 99. Попробуйте снова:")
 
-
-@router.message(Register.phone, F.contact)
+@router.message(Register.phone)
 async def register_phone(message: Message, state: FSMContext):
-    # if Register.phone == message.text:
-    #     await state.update_data(phone=message.text)
-    # elif Register.phone == message.contact:
-    await state.update_data(phone=message.contact.phone_number)
+    await state.update_data(phone=message.text)
+    # await state.update_data(phone=message.contact.phone_number)
     await state.set_state(Register.comment)
     await message.answer('Напишите любой комментарий')
-
 
 
 @router.message(Register.comment)
 async def register_comment(message: Message, state: FSMContext):
     await state.update_data(comment=message.text)
-    data = await state.get_data()
-    await message.answer(f"Ваше имя: {data['name']}\nВаш номер: {data['phone']}\nВаш комментарий: {data['comment']}")
     await message.answer('Последний шаг! Ознакомься с вводными положениями')
     await message.answer_document(document='BQACAgIAAxkBAAPjZovEtgwCQFk7wbxcsxzJJxBmZrkAAndLAAKtI2BIpsSvPCOUhKY1BA')
     await state.set_state(Register.answer)
@@ -78,20 +73,25 @@ async def invalid_yes(message: Message):
 
 @router.message(lambda message: message.text != "/start")
 async def last_message(message: Message, state: FSMContext):
-    await state.clear()
     user_id = message.from_user.id
     if user_states.get(user_id) == 'start_used':
         await message.answer("Спасибо за успешную регистрацию")
         await message.answer_photo(photo='AgACAgIAAxkBAAPTZovCiqUAAUKvX_WkC2eu7cVVa3U1AAI02jEbrSNgSNh2q9HFrniTAQADAgADeQADNQQ')
-        user_states[user_id] = 'no'
+        
+        data = await state.get_data()
+        admin_message = (f"Пользователь оставил комментарий:\nИмя: {data['name']}\nНомер: {data['phone']}\nКомментарий: {data['comment']}")
+        await message.bot.send_message(chat_id=ADMIN_CHAT_ID, text=admin_message)
+        await state.clear()
+
+        user_states[user_id] = 'disabled'
     else:
-        await message.reply("Сначала ознакомьтесь с правилами, используя команду /start.")
+        await message.reply("Чтобы воспользоваться ботом, используйте команду /start.")
     
 
 
 
 
-
+# Это чтобы получить id
 @router.message(F.photo)
 async def get_photo(message: Message):
     await message.answer(f'ID фото: {message.photo[-1].file_id}')
